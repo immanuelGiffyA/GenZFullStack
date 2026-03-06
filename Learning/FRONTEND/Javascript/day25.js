b31d0c95e0ce0138cd110a29a74ef815fabe92cb
immanuelGiffyA <immanuelgiffy643@gmail.com>
2026-03-06 10:53:25 +0530

    Add variable manipulation and logging functionality in day25.js

--- Learning/FRONTEND/Javascript/day25.js
+++ Learning/FRONTEND/Javascript/day25.js
@@ -12,3 +12,20 @@ function click(e){
     
     alert(`your email:${e.target.email.value} and password:${e.target.password.value}`)    
 }
+
+let c=[2,3,4,5,7]
+let d=[8,9,10]
+let e=[...c,...d]
+console.log(e);
+
+let student={}
+let updateStudent ={
+    ...student,
+    mark:450
+}
+let [first,...otherElement]=c
+
+console.log(first,'c -other element');
+console.log(otherElement,'c -otherElement');
+
+
