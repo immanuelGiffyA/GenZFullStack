2d7bf3d114eb697bb24d4cba1f8cedb0a7b4cd82
immanuelGiffyA <immanuelgiffy643@gmail.com>
2026-03-10 09:04:18 +0530

    Add initial HTML structure and JavaScript functionality for Day-26

---
+++ Assignment/Frontend/Javascript/day-26.js
@@ -0,0 +1,52 @@
+function parent(name,callback){
+callback(name);
+}
+function userGreet(jh) {
+    console.log(`hello ${jh}`);
+    
+}
+parent("giffy", userGreet);
+
+
+setTimeout(()=>{
+    console.log('hello');
+    
+},5000)
+
+let id = setInterval(()=>{
+    console.log('apply for each 1s');
+    
+},1000)
+
+setTimeout(()=>{
+    clearInterval(id)
+},5000)
+
+let count= 10
+let countdown = setInterval(()=>{
+    console.log(count)
+    count--
+    if(count<0)
+    clearInterval(countdown)
+},1000)
+
+async function get(){
+    try{
+
+        
+    let res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    data = await res.json()
+console.log(data);
+
+}catch(err){
+    console.log(err);
+}
+
+}
+
+get()
+
+setTimeout(()=>{
+    console.log("Be well");
+    
+},1000);
